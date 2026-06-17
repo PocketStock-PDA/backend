c4994f67ba644fc4f1771afc9e861f70e0718565
uoo6uoow <154813925+uoo6uoow@users.noreply.github.com>
2026-06-17 10:16:16 +0900

    Feat: AssetFeignClient 추가 및 @EnableFeignClients 설정(#11)

--- ledger-api/src/main/java/com/pocketstock/ledger/LedgerApiApplication.java
+++ ledger-api/src/main/java/com/pocketstock/ledger/LedgerApiApplication.java
@@ -3,12 +3,14 @@ package com.pocketstock.ledger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * ledger-api 실행 앱 - DB B(pocketstock_ledger, 원장).
  * cma/exchange/trading 도메인 + user.security(JWT)만 스캔.
  * 회원 도메인(user.member, DB A)은 스캔하지 않아 DB A 결합이 없다.
  */
+@EnableFeignClients(basePackages = "com.pocketstock.ledger.client")
 @SpringBootApplication(scanBasePackages = {
         "com.pocketstock.ledger",
         "com.pocketstock.common",
