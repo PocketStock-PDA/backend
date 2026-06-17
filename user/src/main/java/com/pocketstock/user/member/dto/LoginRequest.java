06139a34d4d3619cc3864aa8cf5b30ee6e15df04
wooji-dev <wooji.dev@gmail.com>
2026-06-17 16:10:58 +0900

    Refactor: 코드래빗 수정사항 반영

--- user/src/main/java/com/pocketstock/user/member/dto/LoginRequest.java
+++ user/src/main/java/com/pocketstock/user/member/dto/LoginRequest.java
@@ -1,4 +1,11 @@
 package com.pocketstock.user.member.dto;
 
 /** ID/PW 로그인 요청. */
-public record LoginRequest(String username, String password) {}
+public record LoginRequest(String username, String password) {
+
+    // 비밀번호는 로그·예외 메시지 유출 방지를 위해 마스킹
+    @Override
+    public String toString() {
+        return "LoginRequest{username=" + username + ", password=***MASKED***}";
+    }
+}
